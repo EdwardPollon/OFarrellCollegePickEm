a44868ce5db65ff2d984b01daa9a9dbf6303212c
EdwardPollon <edwardpollon@gmail.com>
2024-07-16 08:43:50 -0600

    Dialed in functionality for the manageBowlGames LWC

--- force-app/main/default/lwc/manageBowlGames/manageBowlGames.js
+++ force-app/main/default/lwc/manageBowlGames/manageBowlGames.js
@@ -1,18 +1,45 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import importBowlGames from '@salesforce/apex/CollegeFootballCallout.importBowlGames';
 import updateBowlGames from '@salesforce/apex/CollegeFootballCallout.updateBowlGames';
+import deleteBowlGames from '@salesforce/apex/BowlGameUtil.deleteBowlGames';
+import {getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import BOWL_GAMES_IMPORTED_FIELD from '@salesforce/schema/Year__c.Bowl_Games_Imported__c';
 
 export default class ImportBowlGames extends LightningElement {
     @api
     recordId;
-    importDisabled = false;
+    canDeleteBowlGames;
+    @wire(getRecord, { 
+        recordId: '$recordId', 
+        fields: [BOWL_GAMES_IMPORTED_FIELD] 
+    })
+    year;
+
+    get bowlGamesImported() {
+        return getFieldValue(this.year.data, BOWL_GAMES_IMPORTED_FIELD);
+    }
+
+    get cantDeleteBowlGames() {
+        return getFieldValue(this.year.data, BOWL_GAMES_IMPORTED_FIELD) ? false : true;
+    }
 
     importBowlGames() {
         importBowlGames({yearId: this.recordId});
-        this.importDisabled = true;
+        window.location.reload();
     }
 
     updateBowlGames() {
-        
+        //updateBowlGames({yearId: this.recordId});
+        window.location.reload();
+    }
+    
+    importBowlGames() {
+        importBowlGames({yearId: this.recordId});
+        window.location.reload();
+    }
+
+    deleteBowlGames(){
+        deleteBowlGames({yearId: this.recordId});
+        window.location.reload();
     }
 }
